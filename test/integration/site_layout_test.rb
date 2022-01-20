337e125cdc87257441eea6c8adbf61b783f1292d
koll001 <oasis3150@gmail.com>
2022-01-21 02:55:48 +0900

    fix default header

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "サイトURLの確認" do
-    get
+    get root_path
     assert_template 'static_pages/#home'
   end
 end

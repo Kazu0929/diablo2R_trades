cba9e360da59a2102047caef5a81a89558f947d8
koll001 <oasis3150@gmail.com>
2022-01-22 22:00:38 +0900

    Finish users pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録に成功しました。ようこそ！"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  #ストロングパラメーターズ
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Diablo2RTrades
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    #デフォルトの言語指定
+    config.i18n.default_locale = :ja
+
+    # I18nライブラリに訳文の探索場所を指示する
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

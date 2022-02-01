ec0c401749b63550069468dd288d5eb19ffcedc3
koll001 <oasis3150@gmail.com>
2022-02-02 02:54:27 +0900

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,21 @@ Platform.create!(name: "Playstation")
 Platform.create!(name: "Xbox")
 
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "kazu",
-             email: "kazu@gmail.com",
+User.create!(name:  "admin",
+             email: "admin@example.com",
              password:              "112233",
              password_confirmation: "112233",
              platform_ids: [1,2],
              admin: true)
 
 User.create!(name:  "other",
-             email: "other@gmail.com",
+             email: "other@example.com",
              password:              "112233",
              platform_ids:[1],
              password_confirmation: "112233")
 
 User.create!(name:  "ig",
-             email: "ig@gmail.com",
+             email: "ig@example.com",
              password:              "112233",
              platform_ids:[3],
              password_confirmation: "112233")
